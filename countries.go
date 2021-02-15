5ae11887b06e1c8191f7e25adb4496ad8aa850d0
bitcyber <rakysh.inc@gmail.com>
2021-02-16 03:07:50 +0530

    Bank DB WIP

---
+++ countries.go
@@ -0,0 +1,26 @@
+/*
+* CODE GENERATED
+* THIS FILE MUST NOT BE EDITED BY HAND
+ */
+
+package banksdb
+
+// Country represent country code.
+type Country string
+
+// Following constants represent country codes of known banks in db.
+const (
+	AU Country = "au"
+	CA Country = "ca"
+	DE Country = "de"
+	DK Country = "dk"
+	FI Country = "fi"
+	FR Country = "fr"
+	GB Country = "gb"
+	IE Country = "ie"
+	IN Country = "in"
+	NL Country = "nl"
+	NZ Country = "nz"
+	RU Country = "ru"
+	US Country = "us"
+)
